578ac92794601584543992c75d8365d940c20794
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2020-12-30 18:18:56 +0000

    Add "please wait" to star wars character page

--- src/StarWarsCharacterDetail.js
+++ src/StarWarsCharacterDetail.js
@@ -3,16 +3,26 @@ import React from 'react';
 export default class StarWarsCharacterDetail extends React.Component {
     constructor() {
         super();
-        this.state = { character: {} };
+        this.state = { 
+            loading: false,
+            character: {}
+        };
     }
 
     componentDidMount() {
-        fetch(`https:/swapi.dev/api/people/${this.props.id}`)
-            .then(response => response.json())
-            .then(data => this.setState({ character: { name: data.name } }));
+        this.setState({loading: true});
+        setTimeout(() => {
+            fetch(`https:/swapi.dev/api/people/${this.props.id}`)
+                .then(response => response.json())
+                .then(data => this.setState({ loading: false, character: { name: data.name } }));
+        }, 1500);
     }
 
     render() {
+        if (this.state.loading) {
+            return (<div>Please wait...</div>)
+        }
+
         return (
             <div>
                 <h1>{this.state.character.name}</h1>
